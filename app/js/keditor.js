ebcd5e57c53dafbc75829b7f6af9c08b5a0611e3
artnum <etienne@artisan-numerique.ch>
2021-04-23 16:06:03 +0200

    Route message/rcf822 to html
    Should write a parser for mail

--- app/js/keditor.js
+++ app/js/keditor.js
@@ -199,6 +199,7 @@ KEditor.prototype.renderEntry = function (path, entry) {
                 subresolve(htmlnode)
                 return
             case 'text':
+            case 'message':
                 fetch(new URL(this.buildPath(path, entry.id)))
                 .then(response => {
                     if (!response.ok) { resolve(null); return; }
@@ -208,6 +209,7 @@ KEditor.prototype.renderEntry = function (path, entry) {
                         subtype = type.split('/', 2)
                         if (subtype[1] === undefined) { resolve(null); return }
                         switch (subtype[1]) {
+                            case 'rf822':
                             case 'html':
                                 let x = document.createElement('HTML')
                                 x.innerHTML = content
