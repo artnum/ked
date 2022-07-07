42ac5ed9a5831fc712d2b1e3e0863618c2c0a115
artnum <etienne@artisan-numerique.ch>
2022-07-07 14:42:51 +0200

    Fix animator by removing it

--- app/js/anim.js
+++ app/js/anim.js
@@ -1,35 +1,9 @@
 function KEDAnimator() {
-    this.animSpeed = 1000 / 50
-    this.animList = []
 }
 
 KEDAnimator.prototype.push = function (callback) {
-    const p = new Promise((resolve) => {
-        this.animList.push([callback, resolve])
-    })
-    if (!this._run) { this.run() }
-    return p
-}
-    
-KEDAnimator.prototype.run = function () {
-    new Promise((resolve) => {
-        this._run = true
-        window.requestAnimationFrame((start) => {
-            while(performance.now() - start < this.animSpeed) {
-                const op = this.animList.shift()
-                if (op === undefined) { break; }
-                op[0]()
-                op[1]()
-            }
-            resolve()
-        })
-    })
-    .then(() => {
-        if (this.animList.length > 0) {
-            this.run()
-        }
-        this._run = false
+    return new Promise((resolve) => {
+        window.requestAnimationFrame(() => { callback(); resolve() })
     })
 }
-
 const KEDAnim = new KEDAnimator()
